a18c49716322af85efb29aaf0b42a395a7a47b16
ccjohnn <ccjohnn@sina.com>
2016-05-27 16:46:43 +0800

    add login and myutiles

--- app/src/main/java/com/cc/helloworld/MainActivity.java
+++ app/src/main/java/com/cc/helloworld/MainActivity.java
@@ -1,20 +1,37 @@
 package com.cc.helloworld;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
+import com.cc.helloworld.Utiles.MyUtiles;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.gson.Gson;
 
 import org.jdom2.Document;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private List list;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +43,14 @@ public class MainActivity extends AppCompatActivity {
         Document dc = new Document();
         Gson gson = new Gson();
 
+        setWelcomeString();
+
+
+        list = new ArrayList();
+        for (int count = 0; count < 10; count++) {
+            Log.i("MainActivity", "Value" + count);
+
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -35,6 +60,15 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    private void setWelcomeString() {
+        MyUtiles myUtiles = new MyUtiles();
+        TextView tv = (TextView) findViewById(R.id.myView);
+        tv.setText(myUtiles.getValue());
     }
 
     @Override
@@ -57,5 +91,67 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * 注意这里的view，如果要做监听器必须要view，表示绑定
+     * 在哪个view之上的
+     *
+     * @param view
+     */
+    public void addData(View view) {
+        for (int i = 0; i < 1000; i++) {
+            list.add(i);
+        }
+        TextView bt = (TextView) findViewById(R.id.myView);
+        bt.setText("memory:" + list.size());
+
+    }
+
+    public void clearData(View view) {
+
+        list.clear();
+        TextView bt = (TextView) findViewById(R.id.myView);
+        bt.setText("memory:" + list.size());
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.cc.helloworld/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.cc.helloworld/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
---
+++ app/src/main/java/com/cc/helloworld/Utiles/MyUtiles.java
@@ -0,0 +1,10 @@
+package com.cc.helloworld.Utiles;
+
+/**
+ * Created by cc on 2016/4/4.
+ */
+public class MyUtiles {
+    public String getValue() {
+        return "this is from utiles";
+    }
+}
